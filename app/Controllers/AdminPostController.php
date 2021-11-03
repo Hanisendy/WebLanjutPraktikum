4e1e01a69b93990cda949b799d885944be222f13
Hanisendy <62375293+Hanisendy@users.noreply.github.com>
2021-11-03 21:41:19 +0700

    Update AdminPostController.php

--- app/Controllers/AdminPostController.php
+++ app/Controllers/AdminPostController.php
@@ -1,76 +1,111 @@
 <?php
 
-namespace Config;
+namespace App\Controllers;
+use App\Controllers\BaseController;
 
-// Create a new instance of our RouteCollection class.
-$routes = Services::routes();
-
-// Load the system's routing file first, so that the app and ENVIRONMENT
-// can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+class AdminPostsController extends BaseController
 {
-	require SYSTEMPATH . 'Config/Routes.php';
-}
+    public function index()
+    {
+        $PostModel = model("PostModel");
+		$data = [
+			'posts' => $PostModel->findAll()
+		];
+		return view("posts/index", $data);
+    }
 
-/**
- * --------------------------------------------------------------------
- * Router Setup
- * --------------------------------------------------------------------
- */
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-$routes->setAutoRoute(true);
+    public function create()
+    {
+        session();
+        $data = [
+            'validation' => \Config\Services::validation(),
+        ];
+        return view ("posts/create", $data);
+    }
 
-/*
- * --------------------------------------------------------------------
- * Route Definitions
- * --------------------------------------------------------------------
- */
+    public function store()
+    {
+		$valid = $this->validate([
+			"judul" => [
+				"label" => "Judul",
+				"rules" => "required",
+				"errors" => [
+					"required" => "{field} Harus Diisi!"
+				]
+			],
+			"slug" => [
+				"label" => "Slug",
+				"rules" => "required|is_unique[posts.slug]",
+				"errors" => [
+					"required" => "{field} Harus Diisi!",
+					"is_unique" => "{filed} sudah ada!"
+				]
+			],
+			"kategori" => [
+				"label" => "Kategori",
+				"rules" => "required",
+				"errors" => [
+					"{field} Harus Diisi!"
+				]
+			],
+			"author" => [
+				"label" => "Author",
+				"rules" => "required",
+				"errors" => [
+					"{field} Harus Diisi!"
+				]
+			],
+			"deskripsi" => [
+				"label" => "Deskripsi",
+				"rules" => "required",
+				"errors" => [
+					"{field} Harus Diisi!"
+				]
+			]
+		]);
 
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
-$routes->get('/', function (){
-	$data=[
-		'title' => "Blog - Home",
-	];
-	echo view('layouts/header',$data);
-	echo view('layouts/navbar');
-	echo view('v_home');
-	echo view('layouts/footer');
-});
-$routes->get('/admin', 'Templating::index');
-$routes->get('/admin/posts', 'AdminPostController::index');
-$routes->get('/admin/posts/create', 'AdminPostController::create');
-$routes->post('/admin/posts/store', 'AdminPostController::store');
-$routes->get('/posts', 'PostController::index');
-$routes->get('/about', function (){
-	$data=[
-		'title' => "Blog - About",
-	];
-	echo view('layouts/header',$data);
-	echo view('layouts/navbar');
-	echo view('v_about');
-	echo view('layouts/footer');
-});
-$routes->get('/register', 'RegisterController::index');
-$routes->post('/saveRegister', 'RegisterController::saveRegister');
-/*
- * --------------------------------------------------------------------
- * Additional Routing
- * --------------------------------------------------------------------
- *
- * There will often be times that you need additional routing and you
- * need it to be able to override any defaults in this file. Environment
- * based routes is one such time. require() additional route files here
- * to make that happen.
- *
- * You will have access to the $routes object within that file without
- * needing to reload it.
- */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
-{
-	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+		if ($valid) {
+			$data = [
+				'judul' => $this->request->getVar('judul'),
+				'slug' => $this->request->getVar('slug'),
+				'kategori' => $this->request->getVar('kategori'),
+				'author' => $this->request->getVar('author'),
+				'deskripsi' => $this->request->getVar('deskripsi'),
+			];
+
+			$PostModel = model("PostModel");
+			$PostModel -> insert($data);
+			return redirect()->to(base_url('/admin/posts/'));
+		} else {
+			return redirect()->to(base_url('/admin/posts/create'))->withInput()->with('validation', $this->validator);
+		}
+    }
+
+	public function delete($slug)
+	{
+		$PostModel = model("PostModel");
+		$PostModel->where('slug', $slug)->delete();
+		return redirect()->to(base_url('/admin/posts/'));
+		
+	}
+
+	public function edit($slug)
+	{
+		session();
+		$PostModel = model("PostModel");
+        $data = [
+            'validation' => \Config\Services::validation(),
+			'post' => $PostModel->where('slug', $slug)->first()
+        ];
+        return view ("posts/edit", $data);
+	}
+
+
+	public function update($slug)
+	{
+		$PostModel = model("PostModel");
+		$data = $this->request->getPost();
+		$PostModel->update($slug, $data);
+		return redirect()->to(base_url('/admin/posts/'));
+	}
 }
